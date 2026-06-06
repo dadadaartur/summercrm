813968bb6485d55b740e8ad7c18e29315bbae90f
dadadaartur <dadadaartur@gmail.com>
2026-06-06 23:26:29 +0300

    Update deals.js

--- pages/deals.js
+++ pages/deals.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import Link from 'next/link'               // <-- добавлен импорт
+import Link from 'next/link'               // ← обязательно
 import { supabase } from '../lib/supabaseClient'
 
 const STATUS_COLUMNS = [
